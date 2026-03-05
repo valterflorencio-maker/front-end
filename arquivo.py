4cf8495851e8efc90a4f2d747aa96414b574cee2
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-05 11:27:26 -0300

    Document basic Python data types
    Added documentation for basic data types in Python.

---
+++ arquivo.py
@@ -0,0 +1,17 @@
+int (Inteiros): Usado para números inteiros, sem casas decimais.
+
+Exemplos: 7, 0, -4.
+
+float (Flutuantes): Usado para números reais ou números com vírgula (em programação, usa-se o ponto . como separador decimal).
+
+Exemplos: 0.45, 7.6.
+
+bool (Booleanos): Representa valores lógicos: verdadeiro ou falso.
+
+Exemplos: True ou False.
+
+Nota importante: Em Python, a primeira letra deve ser sempre maiúscula.
+
+str (Strings): Sequências de caracteres, palavras ou textos. Devem estar entre aspas simples ou duplas.
+
+Exemplos: 'ola', "7.5".
